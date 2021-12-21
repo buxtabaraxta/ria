b9b8a46ff9b629212c703fa97e233d2bcd878dc4
ria <buxta_baraxta@yahoo.com>
2021-12-21 03:37:36 +0200

    Added urgent search

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ example_api_search = "https://developers.ria.com/auto/search?api_key=YOUR_API&" 
                        "category_id=1&bodystyle=3&bodystyle=2&marka_id=79&model_id=0&" \
                        "s_yers=2010&po_yers=2010"
                        
-# Example search by id
+# Working search example by id
 id19050985 = "https://developers.ria.com/auto/info?api_key=YOUR_API_KEY&auto_id=19050985" 
 
-# Work search example
+# Working search example by parameter 
 # result = requests.get("https://developers.ria.com/auto/search",
 #                       params={'api_key': config.api, 'category_id': 1, "bodystyle": 3, "marka_id": 79,
 #                               "model_id": 0, "s_yers": 2010, "po_years": 2010})
 
 """
-
+auto_ria_domain = "https://auto.ria.com"
 domain_auto = "https://developers.ria.com/auto/search"
 category_id = "category_id/1"
 category_auto = "categories/1"
@@ -35,10 +35,11 @@ category_auto = "categories/1"
 # audi2000 = requests.get("https://developers.ria.com/auto/categories/1/marks/6/", params={'api_key': config.api})
 
 
-all_bmw_745_2002 = requests.get("https://developers.ria.com/auto/search",
-                                params={'api_key': api.key, 'category_id': 1, "marka_id": 9,
-                                        "model_id": 33436, "s_yers": 2002, "po_years": 2002, "abroad": 0, "custom": 1,
-                                        "countpage": 30})
+all_bmw_745_2002_2002 = requests.get("https://developers.ria.com/auto/search",
+                                     params={'api_key': api.key, 'category_id': 1, "marka_id": 9,
+                                             "model_id": 33436, "s_yers": 2002, "po_years": 2002, "abroad": 0,
+                                             "custom": 1,
+                                             "countpage": 30, "page": 0})
 
 bmw_31396593 = f"https://developers.ria.com/auto/info?api_key={api.key}&auto_id=31396593"
 
@@ -47,7 +48,7 @@ def get_url(mark, model):
     return requests.get(f"{domain_auto}/{category_id}/marka_id/{mark}/", params={'api_key': api.key})
 
 
-# Function generates json file. Takes two parameters (endpoint, str(name of file))
+# Function generates json file from RIA parameters. Takes two parameters (endpoint, str(name of file))
 def create_configurator_car_json(endpoint, name):
     with open(name + '.json', 'w', encoding='utf-8') as f:
         # print(endpoint.json()w
@@ -57,28 +58,52 @@ def create_configurator_car_json(endpoint, name):
         f.write(str(human_redable))
 
 
-bmw_31396593 = requests.get(bmw_31396593)
+# bmw_31396593 = requests.get(bmw_31396593)
 
 
 def create_json(endpoint, name):
     with open(name + '.json', 'w', encoding='utf-8') as f:
-        f.write(str(endpoint.json()))
+        # f.write(str(endpoint.json())) - working
+        f.write(json.dumps(endpoint.json()))
 
 
 # Create json with spake two txt files
 """
 keys = []
 values = []
-with open("search_parameters", encoding='utf-8') as file, open("search_parameters_1") as file_1:
+with open("<FILE_NAME>", encoding='utf-8') as file, open("<FILE_NAME>") as file_1:
     for line in file:
         keys.append(line.rstrip())
     for line in file_1:
         values.append(line.rstrip())
 
 parameters = dict(zip(keys, values))
-with open("configurator_car/search_parameters.json", "w", encoding='utf-8') as file_params:
+with open("<FILE_NAME>", "w", encoding='utf-8') as file_params:
     file_params.write(json.dumps(parameters, ensure_ascii=False))
 """
 
+# Runners
 # create_configurator_car_json(None, None)
-create_json(bmw_31396593, "cars_found/bmw_31396593")
+create_json(all_bmw_745_2002_2002, "cars_found/all_bmw_745_2002_2002")
+
+# Get advert's ids
+ids = list
+with open('cars_found/all_bmw_745_2002_2002.json', 'r', encoding='utf8') as bmws:
+    file_data = json.load(bmws)
+    ids = [line for line in file_data['result']['search_result']['ids']]
+
+
+# print(ids)
+
+
+def find_urgent(ids_list):
+    # id19050985 = "https://developers.ria.com/auto/info?api_key=YOUR_API_KEY&auto_id=19050985" -- Example
+    for _id in ids_list:
+        page = requests.get("https://developers.ria.com/auto/info", params={"api_key": api.key, "auto_id": _id}).json()
+        if 'СРОЧНО' in page['autoData']['description']:
+            print(f"{auto_ria_domain}{page['linkToView']}")
+            print('{:.150}'.format(str(page['autoData']['description'])))
+        # print(page['autoData']['description'])
+
+
+find_urgent(ids)
